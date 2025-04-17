6b8aab4f34538f9bea59914359d175f351b3502c
Juan Moreno <juan.moreno.correa@correounivalle.edu.co>
2025-04-16 21:33:44 -0500

    Commit JavaDoc

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,3 +1,9 @@
+/**
+ * Clase principal de la aplicación Sudoku que inicia la interfaz gráfica de usuario.
+ * Esta clase extiende de Application y configura la ventana principal del juego.
+ * @author Juan Moreno (2417575), Miguel Romero (2421802)
+ * @version 1.0
+ */
 package application;
 
 import javafx.application.Application;
@@ -8,6 +14,11 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    /**
+     * Método que inicia la aplicación JavaFX.
+     * Carga la vista FXML y configura la escena principal.
+     * @param primaryStage El escenario principal proporcionado por la plataforma JavaFX
+     */
     @Override
     public void start(Stage primaryStage) {
         try {
@@ -20,6 +31,10 @@ public class Main extends Application {
         }
     }
 
+    /**
+     * Punto de entrada principal de la aplicación.
+     * @param args Argumentos de línea de comandos (no utilizados)
+     */
     public static void main(String[] args) {
         launch(args);
     }
