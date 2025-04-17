5d843dc23038ffa24e24c06e7be610b70d31f5f0
Juan Moreno <juan.moreno.correa@correounivalle.edu.co>
2025-04-16 21:32:40 -0500

    Commit JavaDoc

--- src/main/java/View/SudokuView.java
+++ src/main/java/View/SudokuView.java
@@ -1,3 +1,9 @@
+/**
+ * Clase que representa la vista del tablero de Sudoku.
+ * Extiende de BorderPane para organizar los componentes de la interfaz.
+ * @author Juan Moreno (2417575), Miguel Romero (2421802)
+ * @version 1.0
+ */
 package View;
 
 import javafx.fxml.FXML;
@@ -8,7 +14,10 @@ import javafx.scene.layout.BorderPane;
 import java.io.IOException;
 
 public class SudokuView extends BorderPane {
+    /**
+     * GridPane que contiene las celdas del tablero de Sudoku.
+     * Esta cuadrícula se configura en el archivo FXML asociado.
+     */
     @FXML
     private GridPane gridPane;
-
-    }
+}
